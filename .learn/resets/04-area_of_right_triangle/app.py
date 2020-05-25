f41d851f8600cea5a823ae78227343df18e8b0b2
Hernán García <hernanjkd@gmail.com>
2020-05-25 03:17:49 +0000

    e 4

--- .learn/resets/04-area_of_right_triangle/app.py
+++ .learn/resets/04-area_of_right_triangle/app.py
@@ -1,10 +1,10 @@
 #Complete the function to return the area of the triangle.
-b = int(input("Size of B"))
-h = int(input("Size of H"))
+b = input("Size of B")
+h = input("Size of H")
 
 def area_of_triangle(arg1, arg2):
     #your code here, please remove the "None" 
-    return arg1 * arg2 / 2
+    return None
 
 # Testing your function
 print(area_of_triangle(b, h))
