ea9cf3d1d9faefd68df3429d4fe46da001be4587
Hernán García <hernanjkd@gmail.com>
2020-05-27 03:32:19 +0000

    exer 8

--- .learn/resets/08-hours_and_minutes/app.py
+++ .learn/resets/08-hours_and_minutes/app.py
@@ -1,10 +1,10 @@
 #Complete the function to calculate how many hours and minutes are passed since midnight.
 def hours_minutes(secs):
   
-  return secs//60//60, secs//60
+  return None
 
 
 
 
 #Invoke the funtion and pass any interger as its argument.
-print(hours_minutes(3900))
+print(hours_minutes())
